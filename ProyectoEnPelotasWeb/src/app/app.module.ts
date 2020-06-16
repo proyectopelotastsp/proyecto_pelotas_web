fc6af4065beaef43408651286adfb5859113ad81
cafranco0505 <48168598+cafranco0505@users.noreply.github.com>
2020-06-16 14:42:10 -0500

    nav
    Inclusion de menu nav y componentes -body - footer

--- ProyectoEnPelotasWeb/src/app/app.module.ts
+++ ProyectoEnPelotasWeb/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './paginas/menu/menu.component';
+import { MainBodyComponent } from './paginas/main-body/main-body.component';
+import { FooterComponent } from './paginas/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    MainBodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

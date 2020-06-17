aa242df17900637f5b0957219c4be143db417720
cafranco0505 <48168598+cafranco0505@users.noreply.github.com>
2020-06-16 19:00:31 -0500

    cambios adicionales

--- ProyectoEnPelotasWeb/src/app/paginas/footer/footer.component.ts
+++ ProyectoEnPelotasWeb/src/app/paginas/footer/footer.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  integrantes: Array<string> = [
+    'CLAUDIA ESPERANZA CELIS PALACIO',
+    'WILMER LOPEZ PEÑA',
+    'CARLOS ANDRES FRANCO RESTREPO',
+    'DIANA CAROLINA PARRA MEJIA',
+    'EIDER GIOVANNI VELEZ BENAVIDES',
+    'JOHN EDISON ZAMORA GAMBA'
+  ];
 
   constructor() { }
 

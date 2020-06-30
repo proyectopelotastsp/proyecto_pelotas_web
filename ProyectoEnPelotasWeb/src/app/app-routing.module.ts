66789809030b0629f6ff7da461b27865c574fcf7
cafranco0505 <48168598+cafranco0505@users.noreply.github.com>
2020-06-30 12:11:45 -0500

    Update app-routing.module.ts

--- ProyectoEnPelotasWeb/src/app/app-routing.module.ts
+++ ProyectoEnPelotasWeb/src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component:}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true})],

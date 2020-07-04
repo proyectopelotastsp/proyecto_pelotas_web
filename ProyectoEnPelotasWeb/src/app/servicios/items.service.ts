72d3148795460d04cee939f1c3149ff1965e49b3
Carlos Franco <cafranco0505@gmail.com>
2020-07-03 22:26:06 -0500

    productos filtrados en categorias

--- ProyectoEnPelotasWeb/src/app/servicios/items.service.ts
+++ ProyectoEnPelotasWeb/src/app/servicios/items.service.ts
@@ -8,13 +8,41 @@ import { CartInterface } from '../interfaces/cart.interface';
 })
 export class ItemsService {
   productos: Array<ProductsInterface> = new Array<ProductsInterface>();
+  futbol: Array<ProductsInterface> = new Array<ProductsInterface>();
+  futbolSala: Array<ProductsInterface> = new Array<ProductsInterface>();
+  baloncesto: Array<ProductsInterface> = new Array<ProductsInterface>();
+  tennis: Array<ProductsInterface> = new Array<ProductsInterface>();
+  voleybol: Array<ProductsInterface> = new Array<ProductsInterface>();
+  results: Array<ProductsInterface> = new Array<ProductsInterface>();
   carrito: Array<CartInterface> = new Array<CartInterface>();
   cartCount: number;
 
   constructor( private http: HttpClient) {
-    this.http.get('assets/data/products.json').subscribe( (resp: Array<ProductsInterface>) => {
-      this.productos = resp;
+    this.LoadBalls().then(() => {
+      this.FilterBalls();
     });
+  }
+
+  private LoadBalls(){
+    return new Promise ((resolve, reject) => {
+      this.http.get('assets/data/products.json').subscribe( (resp: Array<ProductsInterface>) => {
+        this.productos = resp;
+        resolve();
+      });
+   });
+  }
+
+  public FilterBalls(){
+    this.futbol = this.productos.filter(product => product.category === 'Futbol');
+    this.futbolSala = this.productos.filter(product => product.category === 'Futbol-sala');
+    this.baloncesto = this.productos.filter(product => product.category === 'Baloncesto');
+    this.tennis = this.productos.filter(product => product.category === 'Tenis');
+    this.voleybol = this.productos.filter(product => product.category === 'Voleiball');
+  }
+
+  public SearchBalls(query: string){
+    this.results = this.productos.filter(product => product.category === query);
+    console.log(this.results);
   }
 }
 

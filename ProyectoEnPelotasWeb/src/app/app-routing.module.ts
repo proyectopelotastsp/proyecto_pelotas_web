679427037eec0a5fbad2a975d3dde87345008c08
Carlos Franco <cafranco0505@gmail.com>
2020-07-03 21:50:43 -0500

    routing categorias

--- ProyectoEnPelotasWeb/src/app/app-routing.module.ts
+++ ProyectoEnPelotasWeb/src/app/app-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainBodyComponent } from './paginas/main-body/main-body.component';
 import { FutbolComponent } from './paginas/categorias/futbol/futbol.component';
-import { PaginaDetalleComponent } from './paginas/pagina-detalle/pagina-detalle.component';
+import { FutbolSalaComponent } from './paginas/categorias/futbol-sala/futbol-sala.component';
+import { BaloncestoComponent } from './paginas/categorias/baloncesto/baloncesto.component';
+import { TennisComponent } from './paginas/categorias/tennis/tennis.component';
+import { VoleybolComponent } from './paginas/categorias/voleybol/voleybol.component';
 
 const routes: Routes = [
   {path: 'inicio', component: MainBodyComponent},
   {path: 'futbol', component: FutbolComponent},
+  {path: 'futbol-sala', component: FutbolSalaComponent},
+  {path: 'baloncesto', component: BaloncestoComponent},
+  {path: 'tennis', component: TennisComponent},
+  {path: 'voleybol', component: VoleybolComponent},
   {path: '**', pathMatch: 'full', redirectTo: 'inicio'}
 ];
 
